0a2929ff93a5b79530006132c3506f2596f8b60d
dmiweb <dimbons@mail.ru>
2025-06-03 21:18:36 +0300

    fix base path vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/todos-mindbox',
+  base: '/todos-react-mindbox',
   test: {
     globals: true,
     environment: 'jsdom',
